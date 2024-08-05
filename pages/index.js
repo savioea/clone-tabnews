2fa38491f8d6699858518e3be2a0735eed47e289
Sávio Emerick <67128458+savioea@users.noreply.github.com>
2024-08-05 15:48:05 +0000

    Mudada a frase do site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Letícia, eu te amo. Se você me ama, dá uma risadinha!</h1>
+    return <h1>Leleca, você merece muito mais que um site! Te amo pra sempre!</h1>
 }
 
 export default Home;
