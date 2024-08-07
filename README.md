43fda5a1084dbbe719484552bdcafc73e64c9a92
Sávio Emerick <67128458+savioea@users.noreply.github.com>
2024-08-07 22:50:24 +0000

    Adicionados scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews1
+
 Implementação do https://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Leleca, você merece muito mais que um site! Te amo pra sempre!</h1>
+  return (
+    <h1>Leleca, você merece muito mais que um site! Te amo pra sempre!</h1>
+  );
 }
 
 export default Home;
